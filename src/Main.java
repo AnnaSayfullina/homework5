6cf3c2a29593926cc25301538aed9ebbb694af48
anna <anna@sayf.su>
2022-12-08 00:21:16 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,17 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
-      // Пишем код для задачи 2
+        int clientOS = 0;
+        int clientDeviceYear = 2014;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
