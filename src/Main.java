a017f400087324064626db7b5972fca54a37842e
anna <anna@sayf.su>
2022-12-08 00:01:07 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    task1();
+    task2();
+}
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+      // Пишем код для задачи 2
     }
 }
