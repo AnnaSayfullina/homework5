1ad0f1842b717c47ed53ddce40066e0d672a5b4e
anna <anna@sayf.su>
2022-12-08 09:42:47 +0300

    task4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     task1();
     task2();
     task3();
+    task4();
 }
 
     public static void task1 () {
@@ -39,6 +40,20 @@ public class Main {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        int deliveryDistance = 101;
+        int deliveryDays = 1;
+        if (deliveryDistance <=20) {
+            System.out.println("Потребуется дней:" + deliveryDays);
+        } else if (deliveryDistance > 20 && deliveryDistance <=60) {
+            System.out.println("Потребуется дней:" + (deliveryDays + 1));
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней:" + (deliveryDays + 2));
+        } else {
+            System.out.println("Потребуется дней:" + (deliveryDays + 3));
         }
     }
 }
