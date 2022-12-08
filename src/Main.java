153765b957ed52a2eaff1ed38c8a2296e715c59f
anna <anna@sayf.su>
2022-12-08 09:30:13 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
     task1();
     task2();
+    task3();
 }
 
     public static void task1 () {
@@ -27,6 +28,17 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        int year = 2012;
+        if ( (year % 400) == 0 ){
+            System.out.println(year + " год является високосным");
+        } else if ( (year % 4) == 0 && (year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
     }
 }
